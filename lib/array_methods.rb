71e226d145bc7feab7acca155ae3ea807e1fba8a
Richard Bobo <richard.bobo@gmail.com>
2020-03-01 16:34:16 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,9 +7,11 @@
 
 def find_element_index(array, value_to_find)
   count = 0 
+  
   while count < array.length do 
-  array.index(value_to_find)
-
+    array.index(value_to_find[count])
+    count += 1 
+  end 
 end
 
 find_element_index(scale,2)
